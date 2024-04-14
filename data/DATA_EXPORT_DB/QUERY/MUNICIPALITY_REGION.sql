6c8e803a0b8ffc25e5c1a9664ee20b2280cde876
Михаил Соколов <sokol.m00@gmail.com>
2024-04-14 20:14:59 +0700

    Rubbish erase

--- data/DATA_EXPORT_DB/QUERY/MUNICIPALITY_REGION.sql
+++
@@ -1,7 +0,0 @@
-SELECT public."Municipality"."Title", public."Municipality"."RegionID", public."Region"."Title" FROM public."Municipality"
-
-LEFT JOIN public."Region" ON public."Municipality"."RegionID" = public."Region"."RegionID"
-
-WHERE public."Municipality"."RegionID" = '32'
-
-ORDER BY "ID" ASC 
